462f57c3632e1dccc42b816dafe256c098215684
박수민 <qkrtnals032@gmail.com>
2025-03-14 12:45:15 +0900

    [FE] 프론트 프로젝트 생성<박수민>

---
+++ FE/src/App.jsx
@@ -0,0 +1,14 @@
+import zeepseek from './assets/logo/zeepseek.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+      <img src={zeepseek} alt="zeepseek 로고" className="logo" />
+    </div>
+    </>
+  )
+}
+
+export default App
