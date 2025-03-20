74e03f396060a2d2e7365c4fa5807496c8a91279
백승훈 <khbak111@naver.com>
2025-03-20 10:52:40 +0900

    [INFRA] ajax 통신 테스트

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -2,19 +2,31 @@ import React from 'react';
 import zeepseek from './assets/logo/zeepseek.png'
 import './App.css'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
   const navigate = useNavigate();
+
+  const sendTestRequest = async () => {
+    try {
+      const response = await axios.get('http://j12e203.p.ssafy.io:8081/test');
+      console.log('Response:', response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
   
   return (
     <>
       <div className="container">
+
       <img
         src={zeepseek}
         alt="zeepseek 로고"
         className="logo"
         onClick={() => navigate("/main")}
       />
+      <button onClick={sendTestRequest}>Send Test Request</button>
     </div>
     </>
   )
