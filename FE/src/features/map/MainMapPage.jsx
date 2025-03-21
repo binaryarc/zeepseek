1e934c071d4a8a7deb670f5ac4e67ff34248dd37
rjs4013 <rjs4013@naver.com>
2025-03-21 16:16:18 +0900

    [FE] import map <하건수>

--- FE/src/features/map/MainMapPage.jsx
+++ FE/src/features/map/MainMapPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../../common/navbar/Navbar"; // 네비게이션 바 컴포넌트
-import Map from "./mainmap/Map.jsx"; // 지도 컴포넌트
+import Map from "./mainmap/Map"; // 지도 컴포넌트
 import RoomList from "./roomlist/RoomList"; // 매물 리스트 컴포넌트
 import "./MainMapPage.css";
 
