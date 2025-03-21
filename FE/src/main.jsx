7a708bf57b6bc727b433c9d9b117eb27b3ac058a
rjs4013 <rjs4013@naver.com>
2025-03-21 12:28:11 +0900

    [FE][feat] 지도 api 구현 <하건수>

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -1,29 +1,30 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
-import MainPage from './features/main/MainPage.jsx';
-import LoginPage from './features/auth/LoginPage.jsx'
+import "./index.css";
+import App from "./App.jsx";
+import MainPage from "./features/main/MainPage.jsx";
+import LoginPage from "./features/auth/LoginPage.jsx";
+import MainMap from "./features/map/mainmap/MainMap.jsx";
+import Map from "./features/map/mainmap/map.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-        <Route path="/" element={<App />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />    </>
+      <Route path="/" element={<App />} />
+      <Route path="/main" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/map" element={<MainMap />} />
+      <Route path="/map2" element={<Map />} />
+    </>
   )
-)
+);
 
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-     <RouterProvider router={router} />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
+);
