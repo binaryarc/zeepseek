780c471d83fa541c2039ce714f158b39741dcefc
전희성 <gmltjd8002@naver.com>
2025-03-24 22:49:34 +0900

    [BE][feat] 로그 추가 및 누락된 어노테이션 추가

--- BE/zeepseek/src/main/java/com/zeepseek/backend/domain/auth/controller/AuthController.java
+++ BE/zeepseek/src/main/java/com/zeepseek/backend/domain/auth/controller/AuthController.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
+import jakarta.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
