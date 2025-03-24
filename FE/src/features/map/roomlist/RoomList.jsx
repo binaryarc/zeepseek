5fc9ebe9610c6fa7b5bf7f0bf60afc0c6a7f20d0
dnjswoc <dnjswoc@gmail.com>
2025-03-24 14:51:25 +0900

    [FE][feat] 지도 페이지 주거 유형 별 버튼 추가 및 AI 추천 슬라이더 생성 <이원재>

--- FE/src/features/map/roomlist/RoomList.jsx
+++ FE/src/features/map/roomlist/RoomList.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import "./RoomList.css";
+import AiRecommend from "./ai_recommend/AiRecommend";
 
 const RoomList = () => {
+
+  const [selectedTab, setSelectedTab] = useState("원룸/투룸");
+
+  const handleTabClick = (tab) => {
+    setSelectedTab(tab)
+  }
+
   // 더미 데이터
   const properties = [
     {
@@ -22,20 +30,59 @@ const RoomList = () => {
       description: "카페거리 근처, 용산역 접근성",
       img: "https://picsum.photos/200/300?random=1",
     },
+    {
+      id: 4,
+      title: "월세 5000/150",
+      description: "카페거리 근처, 용산역 접근성",
+      img: "https://picsum.photos/200/300?random=1",
+    },
+    {
+      id: 5,
+      title: "월세 5000/150",
+      description: "카페거리 근처, 용산역 접근성",
+      img: "https://picsum.photos/200/300?random=1",
+    },
+    {
+      id: 6,
+      title: "월세 5000/150",
+      description: "카페거리 근처, 용산역 접근성",
+      img: "https://picsum.photos/200/300?random=1",
+    },
+    {
+      id: 7,
+      title: "월세 5000/150",
+      description: "카페거리 근처, 용산역 접근성",
+      img: "https://picsum.photos/200/300?random=1",
+    },
   ];
 
   return (
     <div className="room-list">
-      <h2>매물 리스트</h2>
-      {properties.map((room) => (
-        <div key={room.id} className="room-item">
-          <img src={room.img} alt={room.title} />
-          <div>
-            <p className="room-title">{room.title}</p>
-            <p className="room-description">{room.description}</p>
+      <nav className="room-type">
+        {["원룸/투룸", '오피스텔', "주택/빌라", "AI 추천"].map((tab) => (
+          <span
+            key={tab}
+            className={selectedTab === tab ? "active-tab" : ""}
+            onClick={() => handleTabClick(tab)}
+          >
+            {tab}
+          </span>
+        ))}
+      </nav>
+
+      {selectedTab === "AI 추천" ? (
+        <AiRecommend />
+      ) : (
+        properties.map((room) => (
+          <div key={room.id} className="room-item">
+            <img src={room.img} alt={room.title} />
+            <div>
+              <p className="room-title">{room.title}</p>
+              <p className="room-description">{room.description}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
