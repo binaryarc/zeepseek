7a94f5b272f5a351f72ee251a0e913a8dcff4360
이진호 <jjin4363@gmail.com>
2025-03-25 17:43:08 +0900

    ì¶fix : 추천 응답 dto 수정

--- BE/zeepseek/src/main/java/com/zeepseek/backend/domain/recommend/dto/response/RecommendationResponseDto.java
+++ BE/zeepseek/src/main/java/com/zeepseek/backend/domain/recommend/dto/response/RecommendationResponseDto.java
@@ -1,13 +1,17 @@
 package com.zeepseek.backend.domain.recommend.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import com.zeepseek.backend.domain.recommend.dto.RecommendationDto;
 import java.util.List;
 
 @Data
 public class RecommendationResponseDto {
-    private List<RecommendationDto> recommendedProperties;
-    private long totalElements;
+    @JsonProperty("recommended_properties")
+    private List<Integer> recommendedProperties;
+    private int totalElements;
     private int totalPages;
     private int currentPage;
+
+    // getters, setters, constructors 생략
 }
