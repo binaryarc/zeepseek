838aa7082173dd8ae384f54b5ab21c004e12719d
전희성 <gmltjd8002@naver.com>
2025-03-25 11:23:37 +0900

    [FE][fix] 디버깅용 로그 추가

--- FE/src/features/auth/naver/NaverLogin.jsx
+++ FE/src/features/auth/naver/NaverLogin.jsx
@@ -5,8 +5,11 @@ const NaverLogin = () => {
 
     const handleNaverLogin = () => {
         const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${import.meta.env.VITE_NAVER_CLIENT_ID}&state=${import.meta.env.VITE_NAVER_STATE_STRING}&redirect_uri=${import.meta.env.VITE_NAVER_REDIRECT_URI}`;
-
-    window.location.href = NAVER_AUTH_URL;
+        console.log("네이버 로그인 URL:", NAVER_AUTH_URL);
+        console.log("Client ID:", import.meta.env.VITE_NAVER_CLIENT_ID);
+        console.log("State:", import.meta.env.VITE_NAVER_STATE_STRING);
+        console.log("Redirect URI:", import.meta.env.VITE_NAVER_REDIRECT_URI);
+        window.location.href = NAVER_AUTH_URL;
     };
 
 
