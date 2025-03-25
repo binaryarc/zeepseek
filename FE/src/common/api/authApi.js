f85057799fbcdb21ede47aab72e6a4b5f79286f3
전희성 <gmltjd8002@naver.com>
2025-03-25 12:13:48 +0900

    [FE][fix] /api/v1 중복입력 제거

--- FE/src/common/api/authApi.js
+++ FE/src/common/api/authApi.js
@@ -5,7 +5,7 @@ import zeepApi from "./api";
 // OAuth 로그인 (카카오 & 네이버)
 export const oauthLogin = async (authorizationCode, provider) => {
   try {
-    const response = await zeepApi.post("/api/v1/auth/login", {
+    const response = await zeepApi.post("/auth/login", {
       authorizationCode,
       provider,
     });
