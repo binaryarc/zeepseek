32f83c703c1485645202612d401a44addad20d17
백승훈 <khbak111@naver.com>
2025-03-26 14:39:36 +0900

    [BE] 동 비교 추가

--- BE/zeepseek/src/main/java/com/zeepseek/backend/domain/dong/service/CompareService.java
+++ BE/zeepseek/src/main/java/com/zeepseek/backend/domain/dong/service/CompareService.java
@@ -1,6 +1,8 @@
 package com.zeepseek.backend.domain.dong.service;
 
+import com.zeepseek.backend.domain.dong.document.DongCompareDocs;
 import com.zeepseek.backend.domain.dong.document.DongInfoDocs;
+import com.zeepseek.backend.domain.dong.repository.CompareRepository;
 import com.zeepseek.backend.domain.dong.repository.MongoDongRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,12 +12,27 @@ import org.springframework.stereotype.Service;
 public class CompareService {
 
     private final MongoDongRepository mongoDongRepository;
+    private final CompareRepository compareRepository;
     private final GPTSummaryService gptSummaryService;
 
-    public void compareDong(int dong1, int dong2) {
+    public DongCompareDocs compareDong(int dong1, int dong2) {
         DongInfoDocs dongInfo1 = mongoDongRepository.findByDongId(dong1);
         DongInfoDocs dongInfo2 = mongoDongRepository.findByDongId(dong2);
 
+        DongCompareDocs dongCompareDocs = compareRepository.findByCompareId(dong1 + dong2);
+
+        if(dongCompareDocs == null) {
+            String compareSummary = gptSummaryService.getSummaryForDongCompare(dongInfo1, dongInfo2);
+
+            DongCompareDocs dongCompareInfo = new DongCompareDocs();
+            dongCompareInfo.setCompareId(dong1 + dong2);
+            dongCompareInfo.setCompareSummary(compareSummary);
+
+            compareRepository.save(dongCompareInfo);
+            return dongCompareInfo;
+        } else {
+            return dongCompareDocs;
+        }
 
     }
 
