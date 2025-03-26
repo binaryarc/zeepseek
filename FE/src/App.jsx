7bc123fbdaae58393d05008721e8ea8c3f865528
박수민 <qkrtnals032@gmail.com>
2025-03-26 14:10:47 +0900

    [FE][Fix] CSS<박수민>

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -2,32 +2,11 @@ import React from 'react';
 import zeepseek from './assets/logo/zeepseek.png'
 import './App.css'
 import { useNavigate } from 'react-router-dom';
-// import axios from 'axios';
+
 
 function App() {
   const navigate = useNavigate();
 
-  // const sendTestRequest = async () => {
-  //   try {
-  //     const response = await axios.get('/api/test');
-  //     console.log('Response:', response.data);
-  //   } catch (error) {
-  //     console.error('Error:', error);
-  //   }
-  // };
-
-  // const sendTestPost = async () => {
-  //   try {
-  //     const response = await axios.post('/api/test', {
-  //       name: "web",
-  //       age: 111
-  //     });
-  //     console.log('Response:', response.data);
-  //   } catch (error) {
-  //     console.error('Error:', error);
-  //   }
-  // };
-  
   return (
     <>
       <div className="app-container">
