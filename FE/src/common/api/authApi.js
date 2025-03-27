0e3879c6938eb68f316bac78d1b927c0e8a32528
하건수 <rjs4013@naver.com>
2025-03-27 16:52:18 +0900

    Update FE/src/common/api/authApi.js

--- FE/src/common/api/authApi.js
+++ FE/src/common/api/authApi.js
@@ -1,14 +1,4 @@
-import axios from "axios";
-import zeepApi from "./api/zeepApi"
-// import { setAccessToken, setUser } from "../../store/authSlice";
-// import store from "../../store/store";
-// // import { setAuth } from "../../store/slices/authSlice";
-// import store from "../../store/store";
-
-const authApi = axios.create({
-  baseURL: `https://j12e203.p.ssafy.io/api/v1`, // ✅ API 서버 주소
-  withCredentials: true, // ✅ 쿠키 포함 요청
-});
+import zeepApi from "./api";
 
 // OAuth 로그인 (카카오 & 네이버)
 export const oauthLogin = async (authorizationCode, provider) => {
@@ -18,9 +8,6 @@ export const oauthLogin = async (authorizationCode, provider) => {
       provider,
     });
 
-    // Redux 상태 업데이트 (토큰 & 유저 정보 저장)
-    // store.dispatch(setAuth({ accessToken: response.data.accessToken, user: response.data }));
-
     return response.data;
   } catch (error) {
     console.error(`${provider} 로그인 실패:`, error);
@@ -29,12 +16,23 @@ export const oauthLogin = async (authorizationCode, provider) => {
 };
 
 export const loginOAuth = async (authorizationCode, provider) => {
-  return await authApi.post("/auth/login", {
+  return await zeepApi.post("/auth/login", {
     authorizationCode,
     provider,
   });
 };
 
+// 로그아웃
+export const logoutOAuth = async (accessToken) => {
+  console.log("로그아웃 요청 accessToken:", accessToken);
+  return await zeepApi.delete("/auth/sessions", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    withCredentials: true, // HttpOnly 쿠키도 포함되도록
+  })
+};
+
 export const refreshAccessToken = async () => {
-  return await authApi.post("/auth/refresh"); // refresh_token은 쿠키에서 자동 전송됨
+  return await zeepApi.post("/auth/refresh"); // refresh_token은 쿠키에서 자동 전송됨
 };
