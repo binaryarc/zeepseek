47f0edeaa77dc463cc9c0bb4c21bb52fc4242225
이진호 <jjin4363@gmail.com>
2025-04-02 10:40:00 +0900

    test: es 유저 테스트

--- ML/app/config/elasticsearch.py
+++ ML/app/config/elasticsearch.py
@@ -3,6 +3,6 @@ from elasticsearch import Elasticsearch
 def get_es_client():
     return Elasticsearch(
         "http://elasticsearch:9200",
-        basic_auth=("kibana", "2zgdxAdWzbURMTj0YMwu"),
+        basic_auth=("fastapi_user", "e203@Password!"),
         verify_certs=False
     )
