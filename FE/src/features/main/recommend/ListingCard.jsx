ffa8f6a5e79c75929cfaeebc5252f90dcbee73b7
백승훈 <khbak111@naver.com>
2025-04-02 21:57:41 +0900

    [fe] 메인 추천 아이템 css 수정정

---
+++ FE/src/features/main/recommend/ListingCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./ListingCard.css";
+import defaultImg from "../../../assets/logo/512image.png";
+
+const ListingCard = ({ image, altText, price, subInfo, description }) => {
+  return (
+    <li className="main-listing-li">
+      <div className="main-listing-item">
+        {/* 상단 이미지 영역 */}
+        <div className="listing-upside">
+          <div className="listing-img-container">
+            {/* 실제로는 image prop이 있으면 그걸 사용, 없으면 defaultImg를 쓰는 식으로 구현 가능 */}
+            <img
+              src={image || defaultImg}
+              alt={altText}
+              className="listing-img"
+            />
+          </div>
+        </div>
+
+        {/* 하단 정보 영역 */}
+        <div className="listing-info">
+          <h3 className="listing-price">{price}</h3>
+          <p className="listing-subinfo">{subInfo}</p>
+          <p className="listing-desc">{description}</p>
+        </div>
+      </div>
+    </li>
+  );
+};
+export default ListingCard;
