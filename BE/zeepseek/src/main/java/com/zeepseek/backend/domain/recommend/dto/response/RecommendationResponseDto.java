664dae0e1ed6d7043ac9025a5038eede54ce3771
전희성 <gmltjd8002@naver.com>
2025-04-04 12:05:32 +0900

    [Be][fix] 첫 로그인 정보 입력시 법정동 코드 입력 추가

--- BE/zeepseek/src/main/java/com/zeepseek/backend/domain/recommend/dto/response/RecommendationResponseDto.java
+++ BE/zeepseek/src/main/java/com/zeepseek/backend/domain/recommend/dto/response/RecommendationResponseDto.java
@@ -12,7 +12,7 @@ import java.util.List;
 @AllArgsConstructor
 public class RecommendationResponseDto {
 
-//    @JsonProperty("recommended_properties")
+    @JsonProperty("recommended_properties")
     private List<RecommendationDto> recommendedProperties;
 
     private int totalElements;
