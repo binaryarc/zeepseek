70d1265f947a00d74c2f1fd68a3a68c058577bb2
이진호 <jjin4363@gmail.com>
2025-04-08 16:32:34 +0900

    롤백

--- FE/src/store/AuthInitializer.jsx
+++ FE/src/store/AuthInitializer.jsx
@@ -11,26 +11,26 @@ const AuthInitializer = () => {
   useEffect(() => {
     const isAuthenticated = localStorage.getItem("isAuthenticated");
     const hasAccessCookie = document.cookie.includes("refreshtoken");
-    // if (isAuthenticated === "true" && hasAccessCookie) {
-    //   refreshAccessToken()
-    //     .then(async (res) => {
-    //       const accessToken = res.data.accessToken;
-    //       const user = res.data.user;
-    //       dispatch(setAccessToken(accessToken));
-    //       dispatch(setUser(user));
-    //       console.log(user)
-    //       // ✅ user는 없음 → setUser 생략
+    if (isAuthenticated === "true" && hasAccessCookie) {
+      refreshAccessToken()
+        .then(async (res) => {
+          const accessToken = res.data.accessToken;
+          const user = res.data.user;
+          dispatch(setAccessToken(accessToken));
+          dispatch(setUser(user));
+          console.log(user)
+          // ✅ user는 없음 → setUser 생략
 
-    //       // ✅ 찜한 동네 정보 가져오기
-    //     const likedDongRes = await fetchLikedRegions(user.idx);
-    //     dispatch(setDongLikes(likedDongRes.data));
-    //     })
-    //     .catch((err) => {
-    //       console.error("refresh 실패:", err);
-    //       localStorage.removeItem("isAuthenticated");
-    //       dispatch(logout());
-    //     });
-    // }
+          // ✅ 찜한 동네 정보 가져오기
+        const likedDongRes = await fetchLikedRegions(user.idx);
+        dispatch(setDongLikes(likedDongRes.data));
+        })
+        .catch((err) => {
+          console.error("refresh 실패:", err);
+          localStorage.removeItem("isAuthenticated");
+          dispatch(logout());
+        });
+    }
   }, [dispatch]);
 
   return null;
