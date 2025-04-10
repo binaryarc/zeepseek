4700fef0164818cfacae7f9cc4da8f854a770186
이진호 <jjin4363@gmail.com>
2025-04-10 21:24:03 +0900

    캐싱 적용

--- BE/zeepseek/src/main/java/com/zeepseek/backend/ZeepseekApplication.java
+++ BE/zeepseek/src/main/java/com/zeepseek/backend/ZeepseekApplication.java
@@ -3,10 +3,12 @@ package com.zeepseek.backend;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableCaching
 public class ZeepseekApplication {
 
 	public static void main(String[] args) {
